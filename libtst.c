dea8a0bc0e5440c575e9f8353529265d1da1354c
Zju Toe <zjutoe@gmail.com>
2013-05-29 09:50:20 +0800

    test functions

--- libtst.c
+++ libtst.c
@@ -1,16 +1,51 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-typedef struct { uint8_t red, green, blue, alpha; } rgba_pixel;
+/* typedef struct { uint8_t red, green, blue, alpha; } rgba_pixel; */
 
-rgba_pixel * init(int n)
+/* rgba_pixel * init(int n) */
+/* { */
+/* 	int i=0;  */
+/* 	rgba_pixel * buf = (rgba_pixel*)malloc(sizeof(rgba_pixel)*n); */
+/* 	if(buf != NULL) { */
+/* 		for (i=0; i<n; i++) */
+/* 			buf[i].red = 128; */
+/* 	} */
+
+/* 	return buf; */
+/* } */
+
+static struct {
+	int buf_sz;
+	int* buf;
+} g;
+
+int init(int n)
 {
-	int i=0; 
-	rgba_pixel * buf = (rgba_pixel*)malloc(sizeof(rgba_pixel)*n);
-	if(buf != NULL) {
-		for (i=0; i<n; i++)
-			buf[i].red = 128;
-	}
+	g.buf = malloc(n);
+	if (g.buf == NULL) 
+		return -1;
+	g.buf_sz = n;
+	return 0;
+}
 
-	return buf;
+int set_buf(int i, int v)
+{
+	if (i<0 || i>g.buf_sz) 
+		return -1;
+	g.buf[i] = v;
+	return 0;
+}
+
+int get_buf(int i)
+{
+	if (i<0 || i>g.buf_sz) 
+		return -1;
+	return g.buf[i];
+}
+
+int fini()
+{
+	free(g.buf);
+	return 0;
 }
--- tst.lua
+++ tst.lua
@@ -1,21 +1,37 @@
 local ffi = require 'ffi'
 
 ffi.cdef[[
-	    int prog2(char* fname);
-typedef struct { uint8_t red, green, blue, alpha; } rgba_pixel;
-rgba_pixel* init(int n);
+      int init(int);
+      int set_buf(int, int);
+      int get_buf(int);
+      int fini();
 ]]
 
-ffi.load('elf', true)
-ffi.load('bsd', true)
-local libprog2=ffi.load('./libprog2.so')
+-- ffi.load('elf', true)
+-- ffi.load('bsd', true)
+-- local libprog2=ffi.load('./libprog2.so')
 
 local libtst = ffi.load('./libtst.so')
-local img = libtst.init(4)
-print(ffi.typeof(img))
-for i=0, 5 do
-   print(img[i].red)
+libtst.init(10)
+-- print(ffi.typeof(img))
+for i=0, 10 do
+   -- print(img[i].red)
+   libtst.set_buf(i, i*2)
 end
+
+for i=0, 10 do
+   -- print(img[i].red)
+   print(libtst.get_buf(i))
+end
+
+libtst.init(5)
+
+print('-----')
+for i=0, 10 do
+   -- print(img[i].red)
+   print(libtst.get_buf(i))
+end
+
 
 --libprog2.prog2(ffi.new('char[13]', './libprog2.so'))
 
