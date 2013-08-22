8ca432d3fc41d674d6351ee5272fcebf1df24169
Toe Zju <zjutoe@gmail.com>
2013-08-22 21:23:56 +0800

    mem.rd()

--- loadelf.lua
+++ loadelf.lua
@@ -243,6 +243,19 @@ function _m.load(fname)
       -- print("")
    end
 
+   function mem.rd(self, addr)
+      local v0 = self[addr]   or 0
+      local v1 = self[addr+1] or 0
+      local v2 = self[addr+2] or 0
+      local v3 = self[addr+3] or 0
+      if addr % 4 == 0 then
+	 -- FIXME is it faster to use bit ops?
+	 return v0 * 2^24 + v1 * 2^16 + v2 * 2^8 + v3
+      else
+	 return nil		-- FIXME raise an exception?
+      end
+   end
+
    return mem
 end
 
